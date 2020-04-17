1ebb8c6aecb5d0e9aae7cff79393af90c39b2dcf
macoding1997 <macoding@163.com>
2020-04-17 15:12:37 +0800

    3.4.0

--- student/views.py
+++ student/views.py
@@ -7,17 +7,11 @@ from .forms import StudentForm
 # Create your views here.
 
 def index(request):
-    words = Student.objects.all()
+    words = Student.get_all()
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
-            clean_data = form.cleaned_data
-            student = Student()
-            student.name = clean_data['name']
-            student.sex = clean_data['sex']
-            student.qq = clean_data['qq']
-            student.email = clean_data['email']
-            student.save()
+            form.save()
             return  HttpResponseRedirect(reverse('index'))
     else:
         form = StudentForm()
